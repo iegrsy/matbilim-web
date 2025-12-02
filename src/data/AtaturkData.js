226eb49c5886b9adc02d89c5b73136673becee76
Ibrahim Ethem Gursoy <ibrahim@entropywork.com>
2025-12-03 00:40:30 +0300

    Add Atatürk data and styling for new component layout. Implement slider for images and display quotes with responsive design.

---
+++ src/data/AtaturkData.js
@@ -0,0 +1,40 @@
+import ataturk1 from "../assets/ataturk_main1.png";
+import ataturk2 from "../assets/ataturk_main2.png";
+import ataturk3 from "../assets/ataturk_main3.png";
+import ataturk4 from "../assets/ataturk_main4.png";
+import ataturk5 from "../assets/ataturk_main5.png";
+const ataturkData = [
+    {
+      image: ataturk1,
+      alt: "Atatürk 1",
+      quote: "Bilim deyince, onda hakikat diye öne sürdüğü önermelerin pekin olmasını ister; pekinlik ise en mükemmel şekliyle matematikte bulunur. O halde bilim o disiplindir ki; önermeleri matematikle ifade edilir. O zaman matematiği kullanmayan disiplinler bilimin dışında kalacaklardır.",
+      author: "Mustafa Kemal Atatürk",
+    },
+    {
+      image: ataturk2,
+      alt: "Atatürk 2",
+      quote: "Okul sayesinde, okulun vereceği ilim ve fen sayesindedir ki, Türk milleti, Türk sanatı, Türk ekonomisi, Türk şiir ve edebiyatı bütün güzellikleriyle gelişir.",
+      author: "Mustafa Kemal Atatürk",
+    },
+    {
+      image: ataturk3,
+      alt: "Atatürk 3",
+      quote: "Milli eğitimde süratle yüksek bir seviyeye çıkacak olan bir milletin, hayat mücadelesinde maddi ve manevi bütün kudretlerinin artacağı muhakkaktır.",
+      author: "Mustafa Kemal Atatürk",
+    },
+    {
+      image: ataturk4,
+      alt: "Atatürk 4",
+      quote: "Eğitimdir ki, bir milleti ya özgür, bağımsız, şanlı, yüksek bir topluluk halinde yaşatır; ya da esaret ve sefalete terk eder.",
+      author: "Mustafa Kemal Atatürk",
+    },
+    {
+      image: ataturk5,
+      alt: "Atatürk 5",
+      quote: "İlim ve fen çalışmalarının merkezi okuldur. Bundan dolayı okul lazımdır. Okul adını hep beraber hürmetle, saygıyla analım.",
+      author: "Mustafa Kemal Atatürk",
+    },
+  ];
+  
+  export default ataturkData;
+
--- src/pages/Ataturk.js
+++ src/pages/Ataturk.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import soon from '../assets/soon.png';
-
+import ataturkData from "../data/AtaturkData";
+import "./Ataturk.css";
+import Slider from 'react-slick'; // react-slick slider kütüphanesi
+import slider1 from '../assets/ataturk_slide1.png';
+import slider2 from '../assets/ataturk_slide2.png';
+import slider3 from '../assets/ataturk_slide3.png';
 function Ataturk() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: true
+  };
+
   return (
-    <div>
-      <div className="empty-space_init"></div>
-      <div className="empty-space"></div>
-      <section className="container pt-5 pb-5 text-center">
-        <h2>Burada güzel şeyler olacak. Çok Yakında!</h2>
-        {/* Resmi gösteriyoruz */}
-        <img src={soon} alt="Soon" className="centered-image" />
-      </section>
+
+    <div >
+      <Slider {...settings} className="slider-container">
+        <div>
+          <img src={slider1} alt="slider1" className="img-fluid" />
+        </div>
+        <div>
+          <img src={slider2} alt="slider2" className="img-fluid" />
+        </div>
+        <div>
+          <img src={slider3} alt="slider3" className="img-fluid" />
+        </div>
+
+      </Slider>
+      <div className="ataturk-rows">
+        <h1>Mustafa Kemal Atatürk</h1>
+
+        {ataturkData.map((item, index) => {
+          // index çiftse row-left, tekse row-right
+          const rowClass = index % 2 === 0 ? "row-left" : "row-right";
+
+          return (
+            <div className={`row ${rowClass}`} key={index}>
+              <div className="row-image">
+                <img src={item.image} alt={item.alt} />
+              </div>
+              <div className="row-quote">
+                <p className="quote-text">"{item.quote}"</p>
+                <p className="quote-author">{item.author}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
